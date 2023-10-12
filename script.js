188fc423f5500e2dac461d834a628db496297bca
N3-Ricky <115377888+N3-Ricky@users.noreply.github.com>
2023-10-12 22:28:17 +1100

    Revert "Finish Draft"
    This reverts commit a14e40339faf1d7d9ac079ce67c16d699e506883.

--- script.js
+++
@@ -1,8 +0,0 @@
-// For Top Nav Bar Animation
-
-function toggleMenu() {
-    const menu = document.querySelector(".menu-links")
-    const icon = document.querySelector(".burger-icon")
-    menu.classList.toggle("open")
-    icon.classList.toggle("open")
-}
