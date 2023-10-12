a14e40339faf1d7d9ac079ce67c16d699e506883
N3-Ricky <115377888+N3-Ricky@users.noreply.github.com>
2023-10-12 22:23:55 +1100

    Finish Draft

---
+++ script.js
@@ -0,0 +1,8 @@
+// For Top Nav Bar Animation
+
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links")
+    const icon = document.querySelector(".burger-icon")
+    menu.classList.toggle("open")
+    icon.classList.toggle("open")
+}
